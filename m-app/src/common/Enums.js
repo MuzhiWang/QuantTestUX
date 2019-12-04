e27bb2ffd528fb4711b6e33aef26b12b7b413a18
MuzhiWang <MuzhiWang@github.com>
2019-12-03 23:28:31 -0800

    Add stock candle stick chart

---
+++ m-app/src/common/Enums.js
@@ -0,0 +1,6 @@
+
+export const DateType = {
+    ONE_MIN: "1min",
+    FIVE_MIN: "5min",
+    ONE_DAY: "day"
+}
--- m-app/src/component/BasicStockCharts.js
+++ m-app/src/component/BasicStockCharts.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import CanvasJSReact from '../lib/canvasjs.react'
+import Select from 'react-select'
+import {DateType} from '../common/Enums'
 
 var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
@@ -8,33 +10,54 @@ class BasicStockCharts extends React.Component {
     constructor(props) {
         super(props);
 
-        var limit = 50000;
-        var y = 100;    
-        var data = [];
-        var dataSeries = { type: "line" };
-        var dataPoints = [];
+        // var limit = 50000;
+        // var y = 100;    
+        // var data = [];
+        // var dataSeries = { type: "line" };
+        // var dataPoints = [];
     
-        for (var i = 0; i < limit; i += 1) {
-            y += Math.round(Math.random() * 10 - 5);
-            dataPoints.push({
-                x: i,
-                y: y
-            });
-        }
-        dataSeries.dataPoints = dataPoints;
-        data.push(dataSeries);
+        // for (var i = 0; i < limit; i += 1) {
+        //     y += Math.round(Math.random() * 10 - 5);
+        //     dataPoints.push({
+        //         x: i,
+        //         y: y
+        //     });
+        // }
+        // dataSeries.dataPoints = dataPoints;
+        // data.push(dataSeries);
     
-        const spanStyle = {
-            position:'absolute', 
-            top: '10px',
-            fontSize: '20px', 
-            fontWeight: 'bold', 
-            backgroundColor: '#d85757',
-            padding: '0px 4px',
-            color: '#ffffff'
-        }
-    
-        var options = {
+        // const spanStyle = {
+        //     position:'absolute', 
+        //     top: '10px',
+        //     fontSize: '20px', 
+        //     fontWeight: 'bold', 
+        //     backgroundColor: '#d85757',
+        //     padding: '0px 4px',
+        //     color: '#ffffff'
+        // }
+
+        // var options = {
+        //     zoomEnabled: true,
+        //     animationEnabled: true,
+        //     title: {
+        //         text: "Try Zooming - Panning"
+        //     },
+        //     axisY: {
+        //         includeZero: false
+        //     },
+        //     // data: data  // random data
+        // }
+
+        
+        this.state = {
+            options: {},
+            stockId: "000001",
+            startDate: "2019-07-25",
+            endDate: "2019-11-10",
+            dateType: DateType.ONE_MIN
+        };
+
+        this.options = {
             zoomEnabled: true,
             animationEnabled: true,
             title: {
@@ -43,62 +66,135 @@ class BasicStockCharts extends React.Component {
             axisY: {
                 includeZero: false
             },
-            data: data  // random data
+            // data: data  // random data
         }
 
-        this.state = {
-            options: options
+        this.options2  = {
+			theme: "light2", // "light1", "light2", "dark1", "dark2"
+			animationEnabled: true,
+            exportEnabled: true,
+            zoomEnabled: true,
+			title:{
+				text: `${this.state.stockId} Stock Price`
+			},
+			axisX: {
+                interval: 3600,
+			},
+			axisY: {
+				includeZero:false,
+				prefix: "￥",
+				title: "Price (in RMB)"
+			}
+        }
+        
+        this.dateTypeOptions = [
+            { value: DateType.ONE_MIN, label: '1 min', color: '#00B8D9', isFixed: true },
+            { value: DateType.FIVE_MIN, label: '5 min', color: '#0052CC', isFixed: true },
+            { value: DateType.ONE_DAY, label: '1 day', color: '#5243AA', isFixed: true },
+        ];
+
+        this.selectedOption = selectedOption => {
+            this.setState({
+                dateType: selectedOption.value
+            });
+            console.log(`date type: ${this.state.dateType}`)
         };
     }
 
+    componentDidMount() {
+        this.getStock();
+    }
+
     getStock = () => {
-        var options = this.state.options
-        var dataSeries = { type: "line" };
-        var dataPoints = [];
+        var dataSeries = { 
+            type: "candlestick",
+            showInLegend: true,
+            name: this.state.stockId,
+            yValueFormatString: "$###0.00",
+            // xValueFormatString: "MMM-D YYYY HH:mm",
+         };
+        let dataPoints = [];
         dataSeries.dataPoints = dataPoints;
         // const request = async () => { 
-        const response = fetch('http://127.0.0.1:5000/get_stock_df?stock_id=000001&&start_date=2019-07-25&&end_date=2019-09-01')
+        fetch(`http://127.0.0.1:5000/get_stock_df?stock_id=${this.state.stockId}&&start_date=${this.state.startDate}&&end_date=${this.state.endDate}`)
         .then(res => res.json())
         .then(res => {
             console.log("log res: \n\n\n");
             console.log(res);
-            var stockData = []
-            var stockDataPoints = []
-            console.log(res["date"])
-            var stockCount = Object.keys(res["date"]).length
-            for (var i = 0; i < stockCount; ++i) {
+            var stockData = [];
+            let stockDataPoints = [];
+            // console.log(res["date"])
+            let stockCount = Object.keys(res["date"]).length;
+            let date = res["date"];
+            let open = res["open"];
+            let high = res["high"];
+            let low = res["low"];
+            let close = res["close"];
+
+            for (let i = 0; i < stockCount; ++i) {
                 stockDataPoints.push({
-                    x: i,
-                    y: res["close"][i]
+                    // label: new Date(date[i]),
+                    label: date[i],
+                    y: [open[i], high[i], low[i], close[i]]
                 });
             }
             dataSeries.dataPoints = stockDataPoints;
             stockData.push(dataSeries);
-            options.data = stockData;
+            this.options2.data = stockData;
+
+            return this.options2;
+        })
+        .then(options => {
+            this.setState({
+                options: options
+            });
             console.log("here it is")
-            console.log(options);
-            return res;
+            console.log(this.state);
         });
-      
-        console.log("after fetch");
-        console.log(options);
+    }
 
+    changeStockId(event) {
+        this.setState({stockId: event.target.value});
+    }
+    
+    changeStartDate(event) {
+        this.setState({startDate: event.target.value});
+    }
+
+    changeEndDate(event) {
+        this.setState({endDate: event.target.value});
+    }
+
+    changeDateType(selectedOption) {
+        
+        console.log(`select option: ${selectedOption}`);
         this.setState({
-            options: options
+            dateType: selectedOption.value
         });
-
-        console.log("check state options");
-        console.log(this.state.options);
     }
 
     render() {
         return (
               <div>
-                  <CanvasJSChart options = {this.state.options}/>
-                  <button
-                    type="button"
-                    onClick={this.getStock}
-                    >Get stock</button>
+                    <Select
+                        defaultValue={this.dateTypeOptions[0]}
+                        options={this.dateTypeOptions}
+                        // options={groupedOptions}
+                        // formatGroupLabel={formatGroupLabel}
+                        onChange={this.selectedOption}
+                    />
+                    {this.state.options != undefined &&
+                    <CanvasJSChart options = {this.state.options}/>}
+                    <button
+                        type="button"
+                        onClick={this.getStock}
+                        >Get stock</button>
+                    <input type="text" name="stockId" value={this.state.stockId} 
+                    onChange={this.changeStockId.bind(this)} />
+                    <input type="text" name="startDate" value={this.state.startDate} 
+                    onChange={this.changeStartDate.bind(this)} />
+                    <input type="text" name="endDate" value={this.state.endDate} 
+                    onChange={this.changeEndDate.bind(this)} />
               </div>
           );
     }
