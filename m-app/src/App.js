0afb02bc65a7e0f237d950ef8877aa7edd380851
MuzhiWang <wmz669082@gmail.com>
2021-05-31 02:27:47 -0700

    First test linking with VNPY BE

--- m-app/src/App.js
+++ m-app/src/App.js
@@ -3,15 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import BasicStockCharts from './component/BasicStockCharts'
 import TDXTestStockCharts from './component/TDXTestStockCharts'
+import VNPYStockCharts from './component/vnpy/vnpyStockCharts';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
-  return (
-    <div className="App">
-        {/* <BasicStockCharts /> */}
-        <TDXTestStockCharts />
-    </div>
-  );
+    return (
+        <div className="App">
+            {/* <BasicStockCharts /> */}
+            <TDXTestStockCharts />
+            <VNPYStockCharts />
+        </div>
+    );
 }
 
 export default App;
